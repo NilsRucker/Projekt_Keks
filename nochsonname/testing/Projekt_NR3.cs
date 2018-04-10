35f7fac8b3bc3f8e28bc693f594fbf9c4abec169
Nils Rücker <38212514+NilsRucker@users.noreply.github.com>
2018-04-10 08:25:58 +0200

    Testing implementiert

--- nochsonname/testing/Projekt_NR3.cs
+++ nochsonname/testing/Projekt_NR3.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using nochsonname;
+using NUnit.Framework;
 
 namespace testing
 {
+    [TestFixture]
     public class Projekt_NR3
     {
-        
+        [Test, Category ("Akzeptanztest")]
+        public void Zahlen_zwischen_Untergrenze_Obergrenze()
+        {
+            var ergebnis = Projekt_NR.Start_Zahlen_testen(3, 5);
+            Assert.AreEqual(new[] { 3, 4, 5 }, ergebnis.ToArray());
+        }
+        [Test, Category ("Gerüsttest")]
+        public void Anzahl_Zahlen()
+        {
+            var ergebnis = Projekt_NR.Zahlen_testen(-1, 44);
+            Assert.AreEqual(46, ergebnis.Count());
+        }
     }
 }
