e1a71363ce71c9b8a7f701c0029e7d2b860a274b
Nils Rücker <RueckeNi@BAIT.BBW.DD>
2018-04-12 07:39:06 +0200

    Finale Version FizzBuzz
    Nicht in der vorgeschriebenen Zahl beendet.

--- FuzzBizz/FuzzBizz/Program.cs
+++ FuzzBizz/FuzzBizz/Program.cs
@@ -10,19 +10,26 @@ namespace FuzzBizz
     {
         static void Main(string[] args)
         {
-            var dividend = 25;
-            var divisor = 5;
+            var dividend = 15;
+            var divisor = 3;
             var Fizz = dividend % divisor == 0;
             string fuzz = "";
             if (Fizz == true)
             {
                 fuzz = "Fizz";
             }
+
 //TRENLINIE-------------------------------------------
             Dictionary<string, string> CreativeName =
                 new Dictionary<string, string>();
             CreativeName.Add(fuzz, "Fizz");
 
+            Console.WriteLine(CreativeName);
+            foreach (KeyValuePair<string, string> item in CreativeName)
+            {
+                Console.WriteLine("Schlüssel: " + item.Key);
+                Console.WriteLine("Wert: " + item.Value);
+            }
             Console.WriteLine("Geben sie eine Zahl ein");
             int untergrenze = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Geben sie eine weitere Zahl ein");
@@ -31,7 +38,9 @@ namespace FuzzBizz
             var zahlen = new List<int>();
             for (int zahl = untergrenze; zahl <= obergrenze; zahl++)
             {
+                
                 zahlen.Add(zahl);
+
             }
 
             zahlen.ForEach(i => Console.Write("{0}\t", i));
