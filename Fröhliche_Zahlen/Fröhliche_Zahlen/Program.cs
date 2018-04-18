ea22b9e1aa2234ce19c408df48b5d608a1f5119a
Nils Rücker <RueckeNi@BAIT.BBW.DD>
2018-04-18 12:06:01 +0200

    Es geht
    YAAAAAAAAAAAAAAAY

--- Fröhliche_Zahlen/Fröhliche_Zahlen/Program.cs
+++ Fröhliche_Zahlen/Fröhliche_Zahlen/Program.cs
@@ -24,26 +24,55 @@ namespace Fröhliche_Zahlen
     }
     public class DER_ZERSETZER
     {
-       
 
-        public static long[] Zersetze(string Eingabe_des_Benutzers)
+
+        public static string froh_or_no(string Eingabe_des_Benutzers)
         {
-            int AnzahlZahlderZahl = Eingabe_des_Benutzers.Length;
-            long Eingabe = Convert.ToInt32(Eingabe_des_Benutzers);
-            long[] Zersetze = new long[AnzahlZahlderZahl];
-            for (int i = 0; i < AnzahlZahlderZahl; i++)
+            long rechnungszahl = 0;
+            do
             {
-                Zersetze[i] = Convert.ToInt64((Eingabe % Math.Pow(10, i+1) / Math.Pow(10, i)));
-            }
-            for (int i = 0; i < Zersetze.Count(); i++)
-
+                int AnzahlZahlderZahl = Eingabe_des_Benutzers.Length;
+                long Eingabe = Convert.ToInt32(Eingabe_des_Benutzers);
+                long[] Zersetze = new long[AnzahlZahlderZahl];
+                
+                
+                long rechnungszah = 0;
+                for (int i = 0; i < AnzahlZahlderZahl; i++)
+                {
+                    Zersetze[i] = (Eingabe % Convert.ToInt32(Math.Pow(10, Convert.ToDouble(i + 1))) / Convert.ToInt32(Math.Pow(10, Convert.ToDouble(i))));
+                }
+                for (int i = 0; i < Zersetze.Count(); i++)
+                {
+                    rechnungszah = rechnungszah + Convert.ToInt64(Math.Pow(Zersetze[i], 2));
+                }
+                rechnungszahl = rechnungszah;
+                Eingabe_des_Benutzers = Convert.ToString(rechnungszah);
+            } while (rechnungszahl != 4 && rechnungszahl != 1);
+                
+            if (rechnungszahl == 4)
             {
-
-               Eingabe = zahl_zum_rechnen + Convert.ToInt32(Math.Pow(Zahlen_getrent[i], 2));
-
+                string froh_or_no = "traurig :(";
+                return froh_or_no;
             }
-            return Zersetze
-;
+
+            
+            else if (rechnungszahl == 1)
+            {
+                string froh_or_no = "froh :D";
+                return froh_or_no;
+            }
+            else
+            {
+                return "Nils ist doooooorf";
+            }
+
+
+
+
+
+            //}
+
+
         }
 
 
@@ -52,7 +81,7 @@ namespace Fröhliche_Zahlen
             var Eingabe = Program.Eingabe_des_Benutzers();
 
             //Rechner.Rechne(Convert.ToInt32(Eingabe));
-            Console.WriteLine();
+            Console.WriteLine(froh_or_no(Eingabe));
             Console.ReadLine();
         }
         
